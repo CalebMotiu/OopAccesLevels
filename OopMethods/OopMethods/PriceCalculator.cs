1ac9af7e2e9430223e2fec790bd9fcb219389d10
CalebMotiu <caleb.motiu@emmanuel.ro>
2023-11-20 09:48:36 +0200

    lectia numarul 8

---
+++ OopMethods/OopMethods/PriceCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopMethods
+{
+    internal static class PriceCalculator
+    {
+        /// <summary>
+        /// Calculates the product price
+        /// </summary>
+        /// <param name="unitPrice"></param>
+        /// <param name="quantity"></param>
+        /// <param name="vatPercent"></param>
+        /// <returns></returns>
+        public static decimal CalculatePrice(decimal unitPrice, decimal quantity = 1M, decimal vatPercent = 19M)
+        {
+            return unitPrice * quantity * (1 + vatPercent / 100);
+        }
+    }
+}
--- OopMethods/OopMethods/Program.cs
+++ OopMethods/OopMethods/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+           
             ConsoleHelper.Print("test");
 
             ConsoleHelper.Print("label","test");
@@ -14,6 +15,64 @@
 
             int[] array = {1, 2, 3, 4, 5};
             ArrayHelper.IterateAndPrint(array);
+            
+            ConsoltHelper_WithDefaultArgs.Print("messaage1");
+
+            ConsoltHelper_WithDefaultArgs.Print("messaage2","label");
+
+            ConsoltHelper_WithDefaultArgs.Print("messaage3","label",3);
+
+            Console.WriteLine("Price=" + PriceCalculator.CalculatePrice(unitPrice: 10));
+            Console.WriteLine("Price=" + PriceCalculator.CalculatePrice(unitPrice: 15, quantity: 3));
+            Console.WriteLine("Price=" + PriceCalculator.CalculatePrice(unitPrice: 20, quantity: 5 , vatPercent: 19));
+            Console.WriteLine("Price=" + PriceCalculator.CalculatePrice(unitPrice: 20, vatPercent: 19));
+            Console.WriteLine("Price=" + PriceCalculator.CalculatePrice(vatPercent: 14, quantity: 5, unitPrice: 20));
+            Console.WriteLine("Price=" + PriceCalculator.CalculatePrice(14, vatPercent: 20));
+            
+            int a = 10;
+            Console.WriteLine($"Befor increment: {a}");
+            Increment(a);
+            Console.WriteLine($"After increment: {a}");
+            
+
+            Person p = new Person
+            {
+                FirstName = "Ioane",
+                LastName = "florintu"
+            };
+
+            Console.WriteLine($"Befor say hello change name: {p.FirstName} {p.LastName}");
+            SayHello(p);
+            Console.WriteLine($"Befor say hello change name: {p.FirstName} {p.LastName}");
+
+        }
+
+
+        private static void  Increment(int i)
+        {
+            Console.WriteLine($"Befor increment: {i}");
+                i++;
+            Console.WriteLine($"After increment: {i}");
+        }
+
+        private static void SayHello(Person p)
+        {
+            Console.WriteLine($"Befor say hello change name: {p.FirstName} {p.LastName}");
+            p.LastName = "Changed";
+            Console.WriteLine($"Befor say hello change name: {p.FirstName} {p.LastName}");
+        }
+
+        private static void SayHello2(Person p)
+        {
+            Console.WriteLine($"Befor say hello change name: {p.FirstName} {p.LastName}");
+            
+            p = new Person
+            {
+                FirstName = "Ioane",
+                LastName = "Changed"
+            };
+            Console.WriteLine($"Befor say hello change name: {p.FirstName} {p.LastName}");
         }
     }
+
 }
