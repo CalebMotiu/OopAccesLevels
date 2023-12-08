cd12008297fd79ee1b0fe694fd70cc6dc0512114
CalebMotiu <caleb.motiu@emmanuel.ro>
2023-12-08 09:46:44 +0200

    intial setup

---
+++ OopMethods/Lectia1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Mostenire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File f = new File("fisier1", "txt");
+            f.Open();
+
+            ExcelFile excelFile1 = new ExcelFile("Workbook", "2023");
+            excelFile1.Open();
+            excelFile1.PerformCalculation("a = b + c");
+
+            PdfFile pdfFile1 = new PdfFile("Declaratie", true);
+            pdfFile1.Open();
+            pdfFile1.AttemptEdit();
+        }
+    }
+}
